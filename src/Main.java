eac15a56d910a2a88e747201961a9500ae93b6ab
zenisekm <zenisek96@gmail.com>
2024-02-20 21:03:00 +0100

    DOMÁCÍ ÚKOL #2 LEKCE.
    Vše vytvořeno. Zkouška veškerého vypsání. Problém s vypsání funkce boolean. Problém s vypsáním druhého date (funce LocalDate).

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import com.engeto.ja.Guest;
 import com.engeto.ja.Room;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +13,6 @@ public class Main {
 
         Guest guest2 = new Guest("Jan Dvořáček ", LocalDate.of(1995,5,5));
 
-
         System.out.println(guest1.getName() + guest1.getBirthdate());
         System.out.println(guest2.getName() + guest2.getBirthdate());
 
@@ -19,7 +20,29 @@ public class Main {
         Room room2 = new Room(2, 1,true, true, 1000);
         Room room3 = new Room(3, 3,false, true, 2400);
 
+
         // ZDE VYPSAT POKOJE NA OBRAZOVKU
+
+        Booking booking1 = new Booking(1, LocalDate.of(2021,7,19),LocalDate.of(2021,7,26),"Work");
+        Booking booking2 = new Booking(2,LocalDate.of(2021,9,1), LocalDate.of(2021,9,14),"Holiday");
+
+        // SEZNAM
+
+        List<Booking> bookingList = new ArrayList<>();
+        bookingList.add(booking1);
+        bookingList.add(booking2);
+        System.out.println(bookingList.size());
+        System.out.println(bookingList.get(0).getGuestsNumber());
+
+        for (Booking booking : bookingList) {
+            System.out.println("Guest number: " + booking.getGuestsNumber()
+                    + " From Date: " + booking.getReservedTimeFrom()
+                 //   + " To Date: " + booking.setReservedTimeTo());
+                    + " Type of vacation: " + booking.getTypeOfVacation());
+
+
+        }
+
 
 
 
