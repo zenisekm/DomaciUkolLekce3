5c676d912f52d914a4f80e5ab55567af642ce16e
zenisekm <zenisek96@gmail.com>
2024-02-22 17:52:36 +0100

    DOMÁCÍ ÚKOL #3 LEKCE.
    Pokus o vytvoření metod ve třídě BookingManager.

--- src/Main.java
+++ src/Main.java
@@ -64,5 +64,8 @@ public class Main {
 
 
 
+
+
+
     }
 }
---
+++ src/com/engeto/ja/com/engeto/ja/BookingManager.java
@@ -0,0 +1,28 @@
+package com.engeto.ja.com.engeto.ja;
+
+
+import com.engeto.ja.Booking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingManager {
+
+       private List<Booking> booking = new ArrayList<>();
+
+        public void addBooking(Booking newBooking) {
+                booking.add(newBooking);
+        }
+
+        public void clearBookings() {
+                booking.clear();
+        }
+
+        public List<Booking> getBookings()  {
+                return new ArrayList<>(booking);
+        }
+
+        // NETUŠÍM CO JAK VYTVOŘIT getBooking(index)
+
+
+}
